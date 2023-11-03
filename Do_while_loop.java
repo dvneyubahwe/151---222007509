86b4aacfe17d734aaf2ecc2298143cde1237124d
dvneyubahwe <149579068+dvneyubahwe@users.noreply.github.com>
2023-11-03 13:02:21 +0200

    Add files via upload

---
+++ Do_while_loop.java
@@ -0,0 +1,11 @@
+class Book{
+    public static void main (String[]args){
+        int salary=10000;
+        do{
+            salary++;
+            System.out.println(salary);
+        } 
+        while( salary<=200000);
+  }
+    
+}
---
+++ whileloop.java
@@ -0,0 +1,10 @@
+ class whilebook{
+    public static void main (String []arg){
+        int x=10;
+        while(x<=20){
+            System.out.println(x);
+            x++;
+        }
+    }
+    
+}
